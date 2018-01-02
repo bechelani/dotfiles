fd2176d8bcfb2dc2d159190efceedc62512e27a8
Michel Bechelani <michel@bechelani.com>
2018-01-02 15:50:54 -0800

    Update ZSH files

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,23 +8,23 @@ autoload -U $HOME/.dotfiles/functions/*(:t)
 #export LS_COLORS=Gxfxcxdxbxegedabagacad
 #export LSCOLORS=dxfxcxdxbxegedabagacad
 #export LS_COLORS=dxfxcxdxbxegedabagacad
-export LSCOLORS=exfxcxdxbxegedabagacad
-export LS_COLORS=exfxcxdxbxegedabagacad
-export CLICOLOR=true
+#export LSCOLORS=exfxcxdxbxegedabagacad
+#export LS_COLORS=exfxcxdxbxegedabagacad
+#export CLICOLOR=true
 
-autoload colors; colors
+#autoload colors; colors
 
 # The variables are wrapped in \%\{\%\}. This should be the case for every
 # variable that does not contain space.
-for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
-  eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
-  eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
-done
+# for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
+#   eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
+#   eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
+# done
 
-eval RESET='$reset_color'
-export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK
-export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
-export PR_BOLD_WHITE PR_BOLD_BLACK
+# eval RESET='$reset_color'
+# export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK
+# export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
+# export PR_BOLD_WHITE PR_BOLD_BLACK
 
 # Clear LSCOLORS
 #unset LSCOLORS
