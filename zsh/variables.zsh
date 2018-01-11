81f3c19d75e2c1f32487985ac0dd97ad05a68257
Michel Bechelani <Michel@Michels-MacBook-Pro.local>
2018-01-10 19:55:12 -0800

    Refactor macOS .dotfiles

---
+++ zsh/variables.zsh
@@ -0,0 +1,14 @@
+##########################################
+# Core Variables
+##########################################
+export EDITOR='vim'
+export HOMEBREW_NO_ANALYTICS=1
+export GITHUB_USER="bechelani"
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
+export MANPAGER="less -X" # Don’t clear the screen after quitting a manual page
+export EDITOR="vim"
+export TERM="screen-256color"
+export CLICOLOR=1
+export LSCOLORS=dxfxcxdxbxegedabagacad
+export LS_COLORS=dxfxcxdxbxegedabagacad
