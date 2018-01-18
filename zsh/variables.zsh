49da31c69b4c3d22ffa82b860a9330333dafbcae
Michel Bechelani <mbechelani@renovateamerica.com>
2018-01-18 11:45:41 -0800

    Remove GREP_OPTIONS

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -2,6 +2,6 @@
 # Core Variables
 ##########################################
 export EDITOR='vim'
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
+#export GREP_OPTIONS='--color=auto'
+#export GREP_COLOR='1;32'
 export TERM="screen-256color"
