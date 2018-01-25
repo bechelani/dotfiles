9d0e9063e304de36eb143dc1edebb7379640b817
Michel Bechelani <michel@bechelani.com>
2018-01-25 11:55:20 -0800

    Fix LS_COLORS

--- ubuntu/variables.zsh
+++ ubuntu/variables.zsh
@@ -1,1 +1,3 @@
-#EXPORT LS_COLORS='di=33:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+LS_COLORS='di=33:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=33:ow=33'
+export LS_COLORS
+
