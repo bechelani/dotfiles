2e43eea9d0d7956f373e7da37fb1b3679717ee1b
Michel Bechelani <michel@bechelani.com>
2020-03-25 13:11:37 -0700

    updates for azshell

--- configure.sh
+++ configure.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
-# Update pkg lists
-
-echo "Updating package lists..."
-sudo apt-get update
-# zsh install
-echo ''
-echo "Now installing zsh..."
-echo ''
-sudo apt install zsh -y
-
-# Installing git completion
-echo ''
-echo "Now installing git and bash-completion..."
-sudo apt-get install git bash-completion -y
-
-echo ''
-echo "Now configuring git-completion..."
-GIT_VERSION=`git --version | awk '{print $3}'`
-URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
-echo ''
-echo "Downloading git-completion for git version: $GIT_VERSION..."
-if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
-	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
-fi
 
 # oh-my-zsh install
+if [ -d ~/.oh-my-zsh/ ] ; then
+    echo ''
+    echo "oh-my-zsh is already installed..."
+    read -p "Would you like to update oh-my-zsh now?" -n 1 -r
+    echo ''
+
+    if [[ $REPLY =~ ^[Yy]$ ]] ; then
+        cd ~/.oh-my-zsh && git pull
+
+        if [[ $? -eq 0 ]] ; then
+            echo "Update complete..." && cd
+        else
+            echo "Update not complete..." >&2 cd
+        fi
+    fi
+else
+    echo "oh-my-zsh not found, now installing oh-my-zsh..."
+    echo ''
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# oh-my-zsh plugin install
 echo ''
-echo "Now installing oh-my-zsh..."
+echo "Now installing oh-my-zsh plugins..."
 echo ''
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# zsh z
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-z
+
+# zsh completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# syntax highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# powerlevel9k install
+echo ''
+echo "Now installing powerlevel9k..."
+echo ''
+git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/powerlevel9k
+
+# powerlevel 10k install
+echo ''
+echo "Now installing powerlevel10k..."
+echo ''
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Pull down personal dotfiles
 echo ''
@@ -36,16 +58,16 @@ read -p "Do you want to use bechelani's dotfiles? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo ''
-	echo "Now pulling down bechelani dotfiles..."
-	git clone https://github.com/bechelani/dotfiles.git ~/.dotfiles
-	echo ''
-	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-	echo ''
-	echo "Checking out azshell branch..." && git checkout azshell
-	echo ''
-	echo "Setting script to exectubale" && chmod +x $HOME/.dotfiles/script/bootstrap
-	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+    echo ''
+    echo "Now pulling down bechelani dotfiles..."
+    git clone https://github.com/bechelani/dotfiles.git ~/.dotfiles
+    echo ''
+    cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+    echo ''
+    echo "Checking out azshell branch..." && git checkout azshell
+    echo ''
+    echo "Setting script to exectubale" && chmod +x $HOME/.dotfiles/script/bootstrap
+    echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
     if [[ $? -eq 0 ]]
     then
         echo "Successfully configured your environment with bechelani's dotfiles..."
@@ -53,16 +75,16 @@ then
         echo "bechelani's dotfiles were not applied successfully..." >&2
 fi
 else
-	echo ''
-  echo "You chose not to apply bechelani's dotfiles. You will need to configure your environment manually..."
-	echo ''
-	echo "Setting defaults for .zshrc and .bashrc..."
-	echo ''
-	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
-	echo ''
-	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
-	echo ''
-	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
+    echo ''
+    echo "You chose not to apply bechelani's dotfiles. You will need to configure your environment manually..."
+    echo ''
+    echo "Setting defaults for .zshrc and .bashrc..."
+    echo ''
+    echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+    echo ''
+    echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
+    echo ''
+    echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
 fi
 
 echo ''
