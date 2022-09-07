baabbdbde9dea1b95f1ac153dab488f5ac957419
Michel Bechelani <michel@bechelani.com>
2022-09-06 22:59:21 -0700

    update homebrew installation

--- configure.sh
+++ configure.sh
@@ -3,33 +3,76 @@ set -eou pipefail
 
 source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/bechelani/dotfiles/mac/script/prompt)"
 
+xcodeInstall () {
+    # install xcode dependency for homebrew
+
+    info "installing Command Line Tools (CLT) for Xcode"
+    xcode-select --install
+    success "Command Line Tools (CLT) for Xcode installed"
+}
+
 brewInstall () {
-    # Install brew
-    if test ! $(which brew); then
+  # install brew
+
+  if test ! $(which brew); then
     # Install the correct homebrew for each OS type
-        if test "$(uname)" = "Darwin"
-        then
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-            echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
-            eval "$(/opt/homebrew/bin/brew shellenv)"
-            success 'brew installed'
-        elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-        then
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-            success 'brew installed'
-        fi
-    else
-        info 'brew is already installed'
-    fi
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    success "brew installed"
+  else
+    info "brew is already installed"
+  fi
 }
 
 brewUpdate () {
-    brew update
-    success 'brew updated'
+  brew update
+  success "brew updated"
+}
+
+zshInstall () {
+  # zsh install
+  # todo add in check for macOS 10.15 since zsh is default
+  if test $(which zsh); then
+      info "zsh already installed..."
+  else
+      brew install zsh
+      success 'zsh installed'
+  fi
+
+  brew install zsh-completions
+  success 'zsh-completions installed'
+}
+
+zshPluginInstall () {
+    # zsh plugin install
+
+    if [ -d "$HOME/.zsh/zsh-completions" ]; then
+        info 'zsh-completions already installed'
+    else
+        git clone https://github.com/zsh-users/zsh-completions ~/.zsh/zsh-completions && success 'zsh-completions installed'
+    fi
+
+    if [ -d "$HOME/.zsh/zsh-autosuggestions" ]; then
+        info 'zsh-autosuggestions already installed'
+    else
+        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && success 'zsh-autosuggestions installed'
+    fi
+
+    if [ -d "$HOME/.zsh/zsh-syntax-highlighting" ]; then
+        info 'zsh-syntax-highlighting already installed'
+    else
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting && success 'zsh-syntax-highlighting installed'
+    fi
+
+    if [ -d "$HOME/.zsh/zsh-z" ]; then
+      info "zsh-z already exists..."
+    else
+      git clone https://github.com/agkozak/zsh-z ~/.zsh/zsh-z && success "zsh-z installed"
+    fi
 }
 
 ohmyposhInstall () {
     # oh-my-posh install
+
     if test $(which oh-my-posh); then
       info "oh-my-posh is already installed..."
       read -p "Would you like to update oh-my-posh now? y/n " -n 1 -r
@@ -49,105 +92,6 @@ ohmyposhInstall () {
       echo ""
       brew install jandedobbeleer/oh-my-posh/oh-my-posh
       success "oh-my-posh installed"
-    fi
-}
-
-zshInstall () {
-    # zsh install
-    # todo add in check for macOS 10.15 since zsh is default
-    if test $(which zsh); then
-        info "zsh already installed..."
-    else
-        brew install zsh
-        success 'zsh installed'
-    fi
-
-    brew install zsh-completions
-    success 'zsh-completions installed'
-}
-
-zshZInstall () {
-    # Install z for dir searching
-    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-z" ]; then
-        info "zsh-z already exists..."
-    else
-        git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
-        success 'zsh-z installed'
-    fi
-}
-
-ohmyzshInstall () {
-    # oh-my-zsh install
-    if [ -d ~/.oh-my-zsh/ ] ; then
-    info 'oh-my-zsh is already installed...'
-    read -p "Would you like to update oh-my-zsh now? y/n " -n 1 -r
-    echo ''
-        if [[ $REPLY =~ ^[Yy]$ ]] ; then
-        cd ~/.oh-my-zsh && git pull
-            if [[ $? -eq 0 ]]
-            then
-                success "Update complete..." && cd
-            else
-                fail "Update not complete..." >&2 cd
-            fi
-        fi
-    else
-    echo ''
-    echo "oh-my-zsh not found, now installing oh-my-zsh..."
-    echo ''
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-    success 'oh-my-zsh installed'
-    fi
-}
-
-zshPluginInstall () {
-    # zsh plugin install
-    if [ -d "$HOME/.zsh/zsh-completions" ]; then
-        info 'zsh-completions already installed'
-    else
-        git clone https://github.com/zsh-users/zsh-completions ~/.zsh/zsh-completions && success 'zsh-completions installed'
-    fi
-
-    if [ -d "$HOME/.zsh/zsh-autosuggestions" ]; then
-        info 'zsh-autosuggestions already installed'
-    else
-        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && success 'zsh-autosuggestions installed'
-    fi
-
-    if [ -d "$HOME/.zsh/zsh-syntax-highlighting" ]; then
-        info 'zsh-syntax-highlighting already installed'
-    else
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting && success 'zsh-syntax-highlighting installed'
-    fi
-}
-
-pl9kInstall () {
-    # powerlevel9k install
-    if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel9k" ]; then
-        info 'powerlevel9k already installed'
-    else
-        echo "Now installing powerlevel9k..."
-        git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k && success 'powerlevel9k installed'
-    fi
-}
-
-pl10kInstall () {
-    # powerlevel10k install
-    if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
-        info 'powerlevel10k already installed'
-    else
-        echo "Now installing powerlevel10k..."
-        git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k && success 'powerlevel10k installed'
-    fi
-}
-
-tmuxTpmInstall () {
-    # tmux tpm install
-    if [ -d "$HOME/.tmux/plugins/tpm" ]; then
-        info 'tmux tpm already installed'
-    else
-        echo "Now installing Tmux TPM manager..."
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && success 'tmux tpm manager installed'
     fi
 }
 
@@ -190,45 +134,8 @@ dotfilesInstall () {
   fi
 }
 
-vundleInstall () {
-    if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
-        info 'vundle already exists'
-    else
-        # vimrc vundle install
-        echo ''
-        echo "Now installing vundle..."
-        echo ''
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && success 'vundle installed'
-    fi
-}
-
-pathogenInstall () {
-    if [ -f "$HOME/.vim/autoload/pathogen.vim" ]; then
-        info 'pathogen already installed'
-    else
-        mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && success 'pathogen installed'
-    fi
-}
-
-nerdtreeInstall () {
-    if [ -d "$HOME/.vim/bundle/nerdtree" ]; then
-        info 'vim nerdtree already installed'
-    else
-        git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree && success 'vim nerdtree installed'
-    fi
-}
-
-wombatColorSchemeInstall () {
-    if [ -f "$HOME/.vim/colors/wombat.vim" ]; then
-        info "wombat color scheme already installed"
-    else
-        # Vim color scheme install
-        git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat
-        mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
-        success 'vim wombat color scheme installed'
-    fi
-}
+# xcode setup
+xcodeInstall
 
 # brew setup
 brewInstall
@@ -240,19 +147,6 @@ zshPluginInstall
 
 # oh my posh setup
 ohmyposhInstall
-
-# oh my zsh setup
-#ohmyzshInstall
-#zshZInstall
-#pl9kInstall
-#pl10kInstall
-#tmuxTpmInstall
-
-#vim setup
-#vundleInstall
-#pathogenInstall
-#nerdtreeInstall
-#wombatColorSchemeInstall
 
 # dotfiles install
 dotfilesInstall
