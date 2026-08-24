27e29240e40ed0f683fa1d57038591b0084d77b7
Michel Bechelani <michelbechelanib@gmail.com>
2026-08-24 15:13:30 -0400

    chore: update homelab variables

--- homelab/variables.zsh
+++ homelab/variables.zsh
@@ -1,3 +1,4 @@
+export ENV_DIR=~/Documents/Claude/projects/HomeLab
 export PORTAINER_URL=http://192.168.50.10:9000
 export GITOPS_REPO_URL=https://github.com/CloudNinja/homelab.git
 export GITOPS_REPO_REF=refs/heads/main
